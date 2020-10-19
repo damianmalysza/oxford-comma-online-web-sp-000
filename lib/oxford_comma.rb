7694f5fa9cce45190c94c7cd0cbc32409d87b6c8
Damian Malysza <damian.malysza@gmail.com>
2020-10-19 01:55:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return array.join()
   end
   if array.length == 2
-    array[-1] = "and " + array[-1]
+    array[-1] = " and " + array[-1]
     return array.join()
   else
     array[-1] = "and " + array[-1]
